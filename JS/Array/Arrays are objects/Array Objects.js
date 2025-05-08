62729cfa3664236a72afafb5d93b4ba5977752d0
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2025-05-08 15:36:52 -0300

    Intro de arrays como objetos

---
+++ JS/Array/Arrays are objects/Array Objects.js
@@ -0,0 +1,30 @@
+// Criação de array como objeto
+
+var catList = [
+    {
+        name: "Sofa",
+        race: "Calico",
+    },
+    {
+        name: "Ash",
+        race: "Rajado Cinza",
+    }
+];
+
+/*                  Exibindo uma array como objeto
+    Não exibe no Terminal com o Node.js, apenas no console de uma pagina
+*/
+
+console.log(catList);
+
+// Array como objetos e seus metodos
+
+var addCatName = prompt("Digite o nome do gato(a): ");
+var addCatRace = prompt("Digite o nome da raça do gato(a): ");
+catList.push({name:addCatName, race:addCatRace});
+
+// Exibição com forEach
+
+catList.forEach(cat => {
+    console.log(`Nome: ${cat.name} - Raça: ${cat.race}`)
+});
